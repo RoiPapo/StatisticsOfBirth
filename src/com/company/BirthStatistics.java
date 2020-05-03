582e25eb5c511862f5da7347e0a45ecf426ad2c2
YarinBS <yarin10121@gmail.com>
2020-05-03 19:42:48 +0300

    hi

--- src/com/company/BirthStatistics.java
+++ src/com/company/BirthStatistics.java
@@ -50,21 +50,44 @@ public class BirthStatistics {
         return rank;
     }
 
+    public int totalBirths(int year){
+
+    }
+
+    public int getRank(int year, String name, String gender){
+
+    }
+
+    public String getName(int year, int rank, String gender){
+
+    }
+
+    public int yearOfHighestRank(int start_year, int end_year, String name, String gender){
+
+    }
+
+    public int getAverageRank(int start_year, int end_year, String name, String gender){
+
+    }
+
+    public int getTotalBirthsRankedHigher(int year, String name, String gender){
+
+    }
 
     public static void main(String[] args) {
-//        BirthStatistics birthStatistics = new BirthStatistics(args[0]);
+        BirthStatistics birthStatistics = new BirthStatistics(args[0]);
         System.out.println("hello world");
-//        birthStatistics.totalBirths(2010);
-//        int rank = birthStatistics.getRank(2010, "Asher", "M");
-//        System.out.println("Rank is: " + rank);
-//        String name = birthStatistics.getName(2012, 10, "M");
-//        System.out.println("Name: " + name);
-//        System.out.println(birthStatistics.yearOfHighestRank(1880, 2014,"David", "M"));
-//        System.out.println(birthStatistics.yearOfHighestRank(1880, 2014,"Jennifer", "F"));
-//        System.out.println(birthStatistics.getAverageRank(1880, 2014, "Benjamin", "M"));
-//        System.out.println(birthStatistics.getAverageRank(1880,2014, "Lois", "F"));
-//        System.out.println(birthStatistics.getTotalBirthsRankedHigher(2014, "Draco", "M"));
-//        System.out.print(birthStatistics.getTotalBirthsRankedHigher(2014, "Sophia", "F"));
+        birthStatistics.totalBirths(2010);
+        int rank = birthStatistics.getRank(2010, "Asher", "M");
+        System.out.println("Rank is: " + rank);
+        String name = birthStatistics.getName(2012, 10, "M");
+        System.out.println("Name: " + name);
+        System.out.println(birthStatistics.yearOfHighestRank(1880, 2014,"David", "M"));
+        System.out.println(birthStatistics.yearOfHighestRank(1880, 2014,"Jennifer", "F"));
+        System.out.println(birthStatistics.getAverageRank(1880, 2014, "Benjamin", "M"));
+        System.out.println(birthStatistics.getAverageRank(1880,2014, "Lois", "F"));
+        System.out.println(birthStatistics.getTotalBirthsRankedHigher(2014, "Draco", "M"));
+        System.out.print(birthStatistics.getTotalBirthsRankedHigher(2014, "Sophia", "F"));
     }
 
 
